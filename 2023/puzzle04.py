79a30b84f28b27f591e6d98dbddc39e3de8bf95c
Maurice Hendrix <maurice.hendrix@tno.nl>
2023-12-05 09:55:11 +0100

    timings

--- 2023/puzzle04.py
+++ 2023/puzzle04.py
@@ -59,15 +59,15 @@ def part2b(puzzle_input: list[str]):
         card = card.split('|')
         card[0] = list(map(int, card[0].strip().split(' ')))
         card[1] = list(map(int, card[1].strip().split(' ')))
-        cards[card_num] = [card]
+        cards[card_num] = card
         cards_dup[card_num] = 1
     card_num = 1
     while card_num < len(puzzle_input):
         count = 0
         exists = {}
-        for num in cards[card_num][0][0]:
+        for num in cards[card_num][0]:
             exists[num]=0
-        for num in cards[card_num][0][1]:
+        for num in cards[card_num][1]:
             exists[num] = exists.get(num,-1)+1
         count = sum(exists.values())
         for _ in range(cards_dup[card_num]):
@@ -89,15 +89,15 @@ def part2c(puzzle_input: list[str]):
         card = card.split('|')
         card[0] = list(map(int, card[0].strip().split(' ')))
         card[1] = list(map(int, card[1].strip().split(' ')))
-        cards.append([card])
+        cards.append(card)
         cards_dup.append(1)
     card_num = 1
     while card_num < len(puzzle_input):
         count = 0
         exists = {}
-        for num in cards[card_num-1][0][0]:
+        for num in cards[card_num-1][0]:
             exists[num]=0
-        for num in cards[card_num-1][0][1]:
+        for num in cards[card_num-1][1]:
             exists[num] = exists.get(num,-1)+1
         count = sum(exists.values())
         for _ in range(cards_dup[card_num-1]):
@@ -119,24 +119,56 @@ def part2d(puzzle_input: list[str]):
         card = card.split('|')
         card[0] = sorted(list(map(int, card[0].strip().split(' '))))
         card[1] = sorted(list(map(int, card[1].strip().split(' '))))
-        cards.append([card])
+        cards.append(card)
         cards_dup.append(1)
     card_num = 1
     while card_num < len(puzzle_input):
         count = 0
         exists = {}
         i = 0
-        while i < len(cards[card_num-1][0][0]):
+        while i < len(cards[card_num-1][0]):
             j = 0
-            while j < len(cards[card_num-1][0][1]) and i < len(cards[card_num-1][0][0]):
-                if(cards[card_num-1][0][0][i] == cards[card_num-1][0][1][j]):
+            while j < len(cards[card_num-1][1]) and i < len(cards[card_num-1][0]):
+                if(cards[card_num-1][0][i] == cards[card_num-1][1][j]):
                     count += 1
                     i, j = i+1, j+1
-                elif(cards[card_num-1][0][0][i] < cards[card_num-1][0][1][j]):
+                elif(cards[card_num-1][0][i] < cards[card_num-1][1][j]):
                     i += 1
-                elif(cards[card_num-1][0][0][i] > cards[card_num-1][0][1][j]):
+                elif(cards[card_num-1][0][i] > cards[card_num-1][1][j]):
                     j += 1
             i += 1
+        
+        for _ in range(cards_dup[card_num-1]):
+            i = 0
+            while i < count:
+                if i+card_num <= len(puzzle_input):
+                    cards_dup[i+card_num] += 1
+                i += 1
+        card_num += 1
+    return sum(cards_dup)
+
+@timing
+def part2e(puzzle_input: list[str]):
+    cards = []
+    cards_dup = []
+    for card in puzzle_input:
+        score = 0
+        card = card.replace('  ', ' ')
+        card_num = int(re.match('Card *([0-9]+):', card).group(1))
+        card = re.sub(r'Card.*[0-9]+:', '', card)
+        card = card.split('|')
+        card[0] = list(map(int, card[0].strip().split(' ')))
+        card[1] = list(map(int, card[1].strip().split(' ')))
+        cards.append(card)
+        cards_dup.append(1)
+    card_num = 1
+    while card_num < len(puzzle_input):
+        count = 0
+        exists = {}
+        count = 0
+        for num in cards[card_num][0]:
+            if(num in cards[card_num][1]):
+                count += 1
         
         for _ in range(cards_dup[card_num-1]):
             i = 0
@@ -150,7 +182,8 @@ def part2d(puzzle_input: list[str]):
 if __name__ == "__main__":
     puzzle_input = open("input/04.txt").read().splitlines() 
     print(part1(puzzle_input))
-    print(part2(puzzle_input))
-    print(part2b(puzzle_input))
-    print(part2c(puzzle_input))
+    print(part2e(puzzle_input))
     print(part2d(puzzle_input))
+    print(part2c(puzzle_input))
+    print(part2b(puzzle_input))
+    print(part2(puzzle_input))
