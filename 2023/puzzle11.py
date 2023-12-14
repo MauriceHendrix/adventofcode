cd8fb131ad07fe55c2e61a1c7a64f345b2eb1730
Maurice Hendrix <maurice.hendrix@tno.nl>
2023-12-14 16:00:03 +0100

    puzzle 11 part 1

--- 2023/puzzle11.py
+++ 2023/puzzle11.py
@@ -1,13 +1,11 @@
 import itertools
 
 def shortest_path(galaxy_map, pair):
-    print(pair, galaxy_map[pair[0][0]][pair[0][1]], galaxy_map[pair[1][0]][pair[1][1]])
-    return 0
+    return abs(pair[0][0] - pair[1][0]) + abs(pair[0][1] - pair[1][1])
 
 def part1(galaxy_map, galaxy_pos):
     total_sum = 0
     for pair in itertools.combinations(galaxy_pos, 2):
-        print(pair)
         total_sum += shortest_path(galaxy_map, pair)
     return total_sum
 
@@ -34,8 +32,4 @@ if __name__ == "__main__":
         for j, g in enumerate(ln):
             if g == '#':
                 galaxy_pos.append((i,j))            
-    #print(part1(galaxy_map, galaxy_pos))
-    #print(galaxy_map)
-    #print(galaxy_pos)
-
-    print(shortest_path(galaxy_map, ((11,0),(11,5))))
+    print(part1(galaxy_map, galaxy_pos))
