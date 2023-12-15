7f916670a8a4fa3620fb210938bc9fd0ba18d7c5
Maurice Hendrix <maurice.hendrix@tno.nl>
2023-12-15 22:31:32 +0100

    puzzle 12 part 1

--- 2023/puzzle12.py
+++ 2023/puzzle12.py
@@ -1,9 +1,50 @@
-from collections import defaultdict
-#def part1(start):
+from itertools import product
+import re
 
 
-#def part2(start, puzzle_input, adj_mattr):
-
+def find_combos(spring, conti):
+    combos = []
+    for i, p in enumerate(product('.#', repeat = len(spring))):
+        curr_spr = "".join(p)
+        possible = curr_spr.count('#') == sum(conti)
+        if possible:
+            for i, char in enumerate(p):
+                if (spring[i] != '?' and char != spring[i]):
+                    possible = False
+                    break
+            if possible:
+                i = 0
+                for cont in conti:
+                    while i < len(curr_spr) and curr_spr[i] != '#':
+                        i += 1
+                    j = 0
+                    while i < len(curr_spr) and curr_spr[i] == '#':
+                        j += 1
+                        i += 1
+                    if j != cont:
+                        possible = False
+                        break
+                while i < len(curr_spr):
+                    if(curr_spr[i] == '#'):
+                        possible = False
+                        break
+                    i+=1
+            if possible:
+                combos.append(curr_spr)
+    return combos
 
 if __name__ == "__main__":
-    puzzle_input = list(map(list, open("input/12.txt").read().splitlines()))
+    puzzle_input = open("input/12.txt").readlines()
+    springs = []
+    contiguous = []
+    for s in puzzle_input:
+        s = s.strip()
+        sprng, nums = s.split(' ')
+        springs.append(sprng)
+        contiguous.append(tuple(map(int, nums.split(','))))
+
+    count = 0
+    for i in range(len(springs)):
+        count += len(find_combos(springs[i], contiguous[i]))
+    print(count)
+           
