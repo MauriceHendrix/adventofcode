13cf94b6747ee13715e56c016a0478a519343d4d
Maurice Hendrix <maurice.hendrix@tno.nl>
2023-12-19 10:13:57 +0100

    puzzle 13 part 1

--- 2023/puzzle13.py
+++ 2023/puzzle13.py
@@ -1,27 +1,51 @@
-def find_horizontal_reflection(puzzle_input):
-    for i in range(len(puzzle_input)):
-        j=0
-        found = i > 0
-        while j < i and i+1+j < len(puzzle_input):
-            if not puzzle_input[i-j]==puzzle_input[i+1+j]:
-                found = False
-                break
-            j+=1
-        if found:
-            return(i+1, i+2)
+from utility.timing import timing
+from math import ceil
+
+def find_horizontal_reflection(grd):
+    rev_grid = list(reversed(grd))
+    for i in range(ceil(len(grd)/2)):
+        if(grd[:i+1] == list(reversed(grd[i+1:][:i+1]))):
+            return i+1
+        if(rev_grid[:i+1] == list(reversed(rev_grid[i+1:][:i+1]))):
+            return len(grd)-1-i
+    return 0
+        
+def find_horizontal_reflection2(grd):
+    for i in range(len(grd)-1):
+        if(grd[i] == grd[i+1]):
+            found = True
+            for j in range(1, min(i+1, len(grd)-i)):
+                if(0 <= i-j and i+1+j < len(grd)):
+                    if(grd[i-j] != grd[i+1+j]):
+                        found = False
+                        break
+            if found:
+                return i+1
     return 0
 
-def part1(map1, map2):
-    left = min(*find_horizontal_reflection(map1))
-    above = min(*find_horizontal_reflection(map2))
-    return left + 100 * above
+@timing
+def part1(puzzle_input):
+    total_sum = 0
+    for map_input in puzzle_input:
+        grid = list(map(list, map_input.splitlines()))
+        grid_t = [[p[i] for p in grid] for i in range(len(grid[0]))]
+        above = find_horizontal_reflection(grid)
+        left = find_horizontal_reflection(grid_t)
+        total_sum += left + 100 * above
+    return total_sum
+
+@timing
+def part1b(puzzle_input):
+    total_sum = 0
+    for map_input in puzzle_input:
+        grid = list(map(list, map_input.splitlines()))
+        grid_t = [[p[i] for p in grid] for i in range(len(grid[0]))]
+        above = find_horizontal_reflection2(grid)
+        left = find_horizontal_reflection2(grid_t)
+        total_sum += left + 100 * above
+    return total_sum
 
 if __name__ == "__main__":
-    puzzle_input = open("input/13.txt").read().splitlines()
     puzzle_input = open("input/13.txt").read().split('\n\n')
-    map1 = puzzle_input[0].splitlines()
-    map1 = [[p[i] for p in map1] for i in range(len(map1[0]))]
-    map2 = puzzle_input[1].splitlines()
-    left = min(*find_horizontal_reflection(map1))
-    above = min(*find_horizontal_reflection(map2))
-    print(part1(map1, map2))
+    print(part1(puzzle_input))
+    print(part1b(puzzle_input))
