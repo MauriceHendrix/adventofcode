02baa6d2365b0c665634621882e11953bbf2daa6
Maurice Hendrix <maurice.hendrix@tno.nl>
2023-12-22 10:37:13 +0100

    puzzle 13 part b

--- 2023/puzzle13.py
+++ 2023/puzzle13.py
@@ -23,6 +23,22 @@ def find_horizontal_reflection2(grd):
                 return i+1
     return 0
 
+
+def off_by_1(l1, l2):
+    if(len(l1) != len(l2)):
+        return False
+    diff = [[1 if l1[i][j] != l2[i][j] else 0 for j in range(len(l1[i]))] for i in range(len(l1))]
+    return sum(map(sum, diff)) == 1
+
+def find_reflection_part2(grd):
+    rev_grid = list(reversed(grd))
+    for i in range(ceil(len(grd)/2)):
+        if(off_by_1(grd[:i+1], list(reversed(grd[i+1:][:i+1])))):
+            return i+1
+        if(off_by_1(rev_grid[:i+1], list(reversed(rev_grid[i+1:][:i+1])))):
+            return len(grd)-1-i
+    return 0
+
 @timing
 def part1(puzzle_input):
     total_sum = 0
@@ -45,7 +61,18 @@ def part1b(puzzle_input):
         total_sum += left + 100 * above
     return total_sum
 
+def part2(puzzle_input):
+    total_sum = 0
+    for map_input in puzzle_input:
+        grid = list(map(list, map_input.splitlines()))
+        grid_t = [[p[i] for p in grid] for i in range(len(grid[0]))]
+        above = find_reflection_part2(grid)
+        left = find_reflection_part2(grid_t)
+        total_sum += left + 100 * above
+    return total_sum
+    
 if __name__ == "__main__":
     puzzle_input = open("input/13.txt").read().split('\n\n')
     print(part1(puzzle_input))
     print(part1b(puzzle_input))
+    print(part2(puzzle_input))
