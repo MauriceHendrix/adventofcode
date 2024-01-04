597f8d52f1eedcfd02d7fdb578954adb6c391336
Maurice Hendrix <maurice.hendrix@tno.nl>
2024-01-04 22:47:52 +0100

    puzzle 16 part 2

--- 2023/puzzle16.py
+++ 2023/puzzle16.py
@@ -48,10 +48,10 @@ def propagate(beam, energised, ctr):
         return_beams.append(move(i, j, reflect(direction, ctr[i][j])))
     return [b for b in return_beams if b[0]>= 0 and b[1] >= 0 and b[0] <len(ctr[0]) and b[1] < len(ctr)]
 
-def part1(ctr):
+def part1(ctr, start):
     energised = [[False for _ in range(len(ctr[0]))] for __ in range(len(ctr))]
     state_seen = {}
-    beams = [(0, 0, '>')]
+    beams = [start]
     while(len(beams) > 0):
         beam = beams.pop()
         if beam not in state_seen:
@@ -59,7 +59,17 @@ def part1(ctr):
             beams.extend(propagate(beam, energised, ctr))
     return sum(sum(eln) for eln in energised)
 
+def part2(ctr):
+    max_sum = 0
+    #top and bottom edge
+    for j in range(len(ctr[0])):
+        max_sum = max(max_sum, part1(ctr, (0, j, 'v')), part1(ctr, (len(ctr)-1, j, '^')))
+    #left and right edge
+    for i in range(len(ctr[0])):
+        max_sum = max(max_sum, part1(ctr, (i, 0, '>')), part1(ctr, (i, len(ctr[0])-1, '<')))
+    return max_sum
 
 if __name__ == "__main__":
     puzzle_input = open("input/16.txt").read().strip().split('\n')
-    print(part1(puzzle_input))
+    print(part1(puzzle_input, (0, 0, '>')))
+    print(part2(puzzle_input))
